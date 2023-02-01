955dc58b2fd601a18cd376dad6a5c23c2d47e61c
Озод <86515876+SaD-Pr0gEr@users.noreply.github.com>
2023-02-02 01:26:40 +0500

    static and dj-dt urls added

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL)
+    urlpatterns += [path('__debug__/', include('debug_toolbar.urls'))]
