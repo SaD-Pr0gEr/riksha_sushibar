daa9ce52614cfd75fa2e2ff3e5def1d5e6095c63
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2024-02-03 17:09:49 +0500

    django-logging

--- core/settings/base.py
+++ core/settings/base.py
@@ -1,5 +1,8 @@
+import logging.config
+import os
 from pathlib import Path
 
+from django.utils.log import DEFAULT_LOGGING
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -63,6 +66,43 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+LOGGING_CONFIG = None
+LOGLEVEL = os.getenv('LOGLEVEL', 'INFO').upper()
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        },
+        'django.server': DEFAULT_LOGGING['formatters']['django.server'],
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        },
+        'django.server': DEFAULT_LOGGING['handlers']['django.server'],
+    },
+    'loggers': {
+        '': {
+            'level': 'INFO',
+            'handlers': ['console'],
+        },
+        'app': {
+            'level': LOGLEVEL,
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'django.server': DEFAULT_LOGGING['loggers']['django.server'],
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        }
+    },
+})
 
 LANGUAGE_CODE = 'en-us'
 
