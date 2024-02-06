b258fed114f705d774ad414a875939aec080815a
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2024-02-06 21:32:17 +0500

    removed-api

--- core/settings/base.py
+++ core/settings/base.py
@@ -17,7 +17,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
 
     'apps.api.v1.main.apps.MainConfig'
 ]
