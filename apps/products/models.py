f889279e2cfa1180731fde4a1194bd146913d3e1
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2024-02-28 22:36:11 +0500

    fix/views

--- apps/products/models.py
+++ apps/products/models.py
@@ -143,6 +143,7 @@ class Product(models.Model):
     class Meta:
         verbose_name = _('Product')
         verbose_name_plural = _('Products')
+        ordering = ('id',)
 
 
 class ProductIngredient(models.Model):
